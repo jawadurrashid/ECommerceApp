0d0f4f2617de55109a4f40a4490a07c63c533c35
Jawadur Rashid <jawadur.rashid@gmail.com>
2017-12-15 19:59:35 -0500

    Update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var morgan = require('morgan'); //Logs all requests from user
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var User = require('./models/user');
+var ejs = require('ejs');
+var engine = require('ejs-mate')
 
 var app = express();
 mongoose.connect('mongodb://jawadur.rashid:password@ds133796.mlab.com:33796/ecommerceweb', function(err){
@@ -15,21 +17,18 @@ mongoose.connect('mongodb://jawadur.rashid:password@ds133796.mlab.com:33796/ecom
 })
 
 //Middleware
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true}));
+app.engine('ejs', engine);   //Declares engine of choice
+app.set('view engine', 'ejs');
 
-app.post('/create-user', function(req,res,next) {
-    var user = new User();
-    user.email = req.body.email;
-    user.password = req.body.password;
-    user.profile.name = req.body.name;
-    
-    user.save(function(err){
-        if(err) return next(err);        
-        res.json('A new user has been successfully created');
-    });
-});
+var mainRoutes = require('./routes/main')
+var userRoutes = require('./routes/user')
+
+app.use(mainRoutes);
+app.use(userRoutes);
 
 //Validating server
 app.listen(3000,function(err){
